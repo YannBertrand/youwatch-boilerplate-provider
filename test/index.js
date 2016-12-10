7a8024f037831a1b0bc525445110899a7a9f957f
Yann Bertrand <yann@bertrand.com>
2016-12-10 14:14:11 +0100

    Replace BoilerplateProvider var by Provider

--- test/index.js
+++ test/index.js
@@ -1,22 +1,22 @@
 const should = require('should');
 
-const BoilerplateProvider = require('../src/index');
+const Provider = require('../src/index');
 
-describe('BoilerplateProvider', () => {
+describe('Provider', () => {
 
   it('should exists', () => {
-    should(BoilerplateProvider).not.be.null;
+    should(Provider).not.be.null;
   });
 
   describe('#init', () => {
     it('should be defined and be a function', () => {
-      should(BoilerplateProvider.init).not.be.null;
-      should(BoilerplateProvider.init).be.a.Function;
+      should(Provider.init).not.be.null;
+      should(Provider.init).be.a.Function;
     });
 
     describe('#init()', () => {
       it('should throw an error if no argument are passed', () => {
-        should(BoilerplateProvider.init).throw(Error);
+        should(Provider.init).throw(Error);
       });
     });
 
@@ -24,26 +24,26 @@ describe('BoilerplateProvider', () => {
       it('should call the callback when arguments are present', (done) => {
         const config = { get: () => {}, set: () => {} };
 
-        BoilerplateProvider.init(done, config);
+        Provider.init(done, config);
       });
     });
   });
 
   describe('#refreshSubscriptions', () => {
     it('should be defined and be a function', () => {
-      should(BoilerplateProvider.refreshSubscriptions).not.be.null;
-      should(BoilerplateProvider.refreshSubscriptions).be.a.Function;
+      should(Provider.refreshSubscriptions).not.be.null;
+      should(Provider.refreshSubscriptions).be.a.Function;
     });
 
     describe('#refreshSubscriptions()', () => {
       it('should throw an error if no argument are passed', () => {
-        should(BoilerplateProvider.refreshSubscriptions).throw(Error);
+        should(Provider.refreshSubscriptions).throw(Error);
       });
     });
 
     describe('#refreshSubscriptions(callback)', () => {
       it('should call the callback', (done) => {
-        BoilerplateProvider.refreshSubscriptions((err) => {
+        Provider.refreshSubscriptions((err) => {
           should(err).be.undefined;
           done();
         });
@@ -53,19 +53,19 @@ describe('BoilerplateProvider', () => {
 
   describe('#refreshVideos', () => {
     it('should be defined and be a function', () => {
-      should(BoilerplateProvider.refreshVideos).not.be.null;
-      should(BoilerplateProvider.refreshVideos).be.a.Function;
+      should(Provider.refreshVideos).not.be.null;
+      should(Provider.refreshVideos).be.a.Function;
     });
 
     describe('#refreshVideos()', () => {
       it('should throw an error if no argument are passed', () => {
-        should(BoilerplateProvider.refreshVideos).throw(Error);
+        should(Provider.refreshVideos).throw(Error);
       });
     });
 
     describe('#refreshVideos(callback)', () => {
       it('should call the callback', (done) => {
-        BoilerplateProvider.refreshVideos((err) => {
+        Provider.refreshVideos((err) => {
           should(err).be.undefined;
           done();
         });
@@ -75,19 +75,19 @@ describe('BoilerplateProvider', () => {
 
   describe('#refreshVideos', () => {
     it('should be defined and be a function', () => {
-      should(BoilerplateProvider.refreshVideos).not.be.null;
-      should(BoilerplateProvider.refreshVideos).be.a.Function;
+      should(Provider.refreshVideos).not.be.null;
+      should(Provider.refreshVideos).be.a.Function;
     });
 
     describe('#refreshVideos()', () => {
       it('should throw an error if no argument are passed', () => {
-        should(BoilerplateProvider.refreshVideos).throw(Error);
+        should(Provider.refreshVideos).throw(Error);
       });
     });
 
     describe('#refreshVideos(callback)', () => {
       it('should call the callback', (done) => {
-        BoilerplateProvider.refreshVideos((err) => {
+        Provider.refreshVideos((err) => {
           should(err).be.undefined;
           done();
         });
@@ -97,7 +97,7 @@ describe('BoilerplateProvider', () => {
     describe('#refreshVideos(callback, channel)', () => {
       it('should accept an optional channel parameter', (done) => {
         const channel = { id: 1 };
-        BoilerplateProvider.refreshVideos((err) => {
+        Provider.refreshVideos((err) => {
           should(err).be.undefined;
           done();
         }, channel);
@@ -107,19 +107,19 @@ describe('BoilerplateProvider', () => {
 
   describe('#getVideos', () => {
     it('should be defined and be a function', () => {
-      should(BoilerplateProvider.getVideos).not.be.null;
-      should(BoilerplateProvider.getVideos).be.a.Function;
+      should(Provider.getVideos).not.be.null;
+      should(Provider.getVideos).be.a.Function;
     });
 
     describe('#getVideos()', () => {
       it('should throw an error if no argument are passed', () => {
-        should(BoilerplateProvider.getVideos).throw(Error);
+        should(Provider.getVideos).throw(Error);
       });
     });
 
     describe('#getVideos(callback)', () => {
       it('should call the callback', (done) => {
-        BoilerplateProvider.getVideos((err) => {
+        Provider.getVideos((err) => {
           should(err).be.undefined;
           done();
         });
@@ -130,7 +130,7 @@ describe('BoilerplateProvider', () => {
       it('should accept an optional channel parameter', (done) => {
         const channel = { id: 1 };
 
-        BoilerplateProvider.getVideos((err) => {
+        Provider.getVideos((err) => {
           should(err).be.undefined;
           done();
         }, channel);
@@ -140,19 +140,19 @@ describe('BoilerplateProvider', () => {
 
   describe('#getOlderVideos', () => {
     it('should be defined and be a function', () => {
-      should(BoilerplateProvider.getOlderVideos).not.be.null;
-      should(BoilerplateProvider.getOlderVideos).be.a.Function;
+      should(Provider.getOlderVideos).not.be.null;
+      should(Provider.getOlderVideos).be.a.Function;
     });
 
     describe('#getOlderVideos()', () => {
       it('should throw an error if no argument are passed', () => {
-        should(BoilerplateProvider.getOlderVideos).throw(Error);
+        should(Provider.getOlderVideos).throw(Error);
       });
     });
 
     describe('#getOlderVideos(callback)', () => {
       it('should call the callback', (done) => {
-        BoilerplateProvider.getOlderVideos((err) => {
+        Provider.getOlderVideos((err) => {
           should(err).be.undefined;
           done();
         });
@@ -163,7 +163,7 @@ describe('BoilerplateProvider', () => {
       it('should accept an optional video parameter', (done) => {
         const video = { id: 1 };
 
-        BoilerplateProvider.getOlderVideos((err) => {
+        Provider.getOlderVideos((err) => {
           should(err).be.undefined;
           done();
         }, video);
@@ -173,13 +173,13 @@ describe('BoilerplateProvider', () => {
 
   describe('#videoProgress', () => {
     it('should be defined and be a function', () => {
-      should(BoilerplateProvider.videoProgress).not.be.null;
-      should(BoilerplateProvider.videoProgress).be.a.Function;
+      should(Provider.videoProgress).not.be.null;
+      should(Provider.videoProgress).be.a.Function;
     });
 
     describe('#videoProgress()', () => {
       it('should throw an error if no argument are passed', () => {
-        should(BoilerplateProvider.videoProgress).throw(Error);
+        should(Provider.videoProgress).throw(Error);
       });
     });
 
@@ -192,9 +192,9 @@ describe('BoilerplateProvider', () => {
         const video = { id: 1 };
         const time = '08:00';
 
-        should(BoilerplateProvider.videoProgress.bind(null, callback)).throw(Error);
-        should(BoilerplateProvider.videoProgress.bind(null, callback, video)).throw(Error);
-        should(BoilerplateProvider.videoProgress.bind(null, callback, null, time)).throw(Error);
+        should(Provider.videoProgress.bind(null, callback)).throw(Error);
+        should(Provider.videoProgress.bind(null, callback, video)).throw(Error);
+        should(Provider.videoProgress.bind(null, callback, null, time)).throw(Error);
       });
     });
 
@@ -203,7 +203,7 @@ describe('BoilerplateProvider', () => {
         const video = { id: 1 };
         const time = '08:00';
 
-        BoilerplateProvider.videoProgress((err) => {
+        Provider.videoProgress((err) => {
           should(err).be.undefined;
           done();
         }, video, time);
